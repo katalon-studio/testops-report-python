abd88d21886255bb71ba68631c8fe7e7c0982004
Giang Tong <giang.tong@katalon.com>
2021-02-25 10:58:56 +0700

    update package info & git action

--- setup.py
+++ setup.py
@@ -4,19 +4,20 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="katalon-testops-reporter",
-    version="0.0.13",
-    author="Giang Tong",
-    author_email="giangtong@katalon.com",
+    name="testops-reporter",
+    version="1.0.0",
+    author="Katalon, LLC. (https://www.katalon.io)",
+    author_email="info@katalon.io",
     description="Katalon TestOps PyTest Plugin",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/pypa/sampleproject",
+    url="https://github.com/katalon-studio/testops-report-python",
     packages=setuptools.find_packages(),
     classifiers=[
+        "Katalon",
+        "TestOps"
         "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    python_requires='>=3.9',
+    python_requires='>=3.6',
 )
